1f7f71de6b76d4da33e4c9a38de5c257d77efd10
Filip Dominec <filip.dominec@gmail.com>
2016-07-26 15:03:07 +0200

    needed for setting vpn

--- settings.sh
+++ settings.sh
@@ -7,7 +7,7 @@ echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select tr
 echo wicd-daemon wicd/users multiselect `whoami` | debconf-set-selections ## TESTING
 
 ## Basics
-sudo apt-get install -y vim-gtk  ack-grep htop  miredo cstocs testdisk git gitg gnupg  unrar n2n nmap debfoster qemu osdclock baobab wicd unetbootin mc arbtt xdotool xsel nethogs arandr osdsh libxosd2 libnotify-bin
+sudo apt-get install -y vim-gtk  ack-grep htop  miredo cstocs testdisk git gitg gnupg  unrar n2n nmap debfoster qemu osdclock baobab wicd unetbootin mc arbtt xdotool xsel nethogs arandr osdsh libxosd2 libnotify-bin network-manager-pptp
 sudo apt-get install -y default-jre # default-jdk
 sudo apt-get install -y mtpfs mtp-tools gmtp 
 # if it does not help: libmtp-common mtp-tools libmtp-runtime libmtp9
@@ -20,7 +20,7 @@ xgamma -gamma .7
 sudo apt-get install -y pidgin linphone youtube-dl
 
 ## Graphics and writing
-sudo apt-get install -y libreoffice-calc libreoffice-writer libreoffice-impress myspell-dictionary-cs hyphen-cs libreoffice-grammarcheck-cs mythes-cs libreoffice-l10n-cs libreoffice-gtk3 libreoffice-style-tango libreoffice-pdfimport 
+sudo apt-get install -y libreoffice-calc libreoffice-writer libreoffice-impress myspell-dictionary-cs hyphen-cs libreoffice-l10n-cs libreoffice-gtk3 libreoffice-style-tango libreoffice-pdfimport 
 #TODO E: Package 'libreoffice-grammarcheck-cs' has no installation candidate
 #TODO E: Package 'mythes-cs' has no installation candidate
 ## Do not forget to change saving to DOCX/XLSX
@@ -33,7 +33,7 @@ sudo apt-get install -y sound-juicer lame smplayer vlc audacity ffmpeg handbrake
 
 ## Programming, electronics and research
 sudo apt-get install -y avr-libc gcc-avr glade avrdude geda-utils  ## programming and technology
-sudo apt-get install -y ipython python-numpy python3-numpy python-sympy python-matplotlib  ## computing and plotting
+sudo apt-get install -y ipython python-numpy python3-numpy python-sympy python-matplotlib python3-matplotlib python3-pip  ## computing and plotting
 sudo apt-get install -y mpb harminv python-h5py paraview		## electromagnetic computation (MEEP will be compiled from scratch, search for python-meep-install on github)
 
 ## === Remove unused default apps ===
@@ -110,7 +110,7 @@ cd ~/.vim/bundle/ && git clone https://github.com/jaxbot/semantic-highlight.vim.
 ## === Notes to manual settings ===
 
 echo "Important files that need to be transferred (No, I will not add them to this git repo ;)"
-echo "Move to the old home and run these commands:\n"
+echo "Change working directory to the old home and run these commands:\n"
 echo '  OLDHOME=.'
 echo '  NEWHOME=~'
 echo '  cp ${OLDHOME}/.gitconfig ${NEWHOME} '
