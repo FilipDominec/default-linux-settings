d22816d2f7a508b4671add4771d475eb8ca79a12
Filip Dominec <filip.dominec@gmail.com>
2019-05-10 11:34:39 +0200

    object model for matplotlib plotting

--- files/vim/python-template.py
+++ files/vim/python-template.py
@@ -14,19 +14,24 @@ matplotlib.rc('text', usetex=True)
 matplotlib.rc('font', size=8)
 matplotlib.rc('text.latex', preamble = '\usepackage{amsmath}, \usepackage{yfonts}, \usepackage{txfonts}, \usepackage{lmodern},')
 
-# TODO use object model
-#fig.canvas.mpl_connect('close_event', handle_close)
 
+# == Plotting with object model==
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(7, 7))
+#ax.plot(x, y); 
 
-
-## Simple axes
-plt.ylim((-0.1,1.1)); plt.yscale('linear')
-plt.xlim((-0.1,1.1)); plt.xscale('linear')
 
 ## ==== Outputting ====
-## Finish the plot + save 
-plt.xlabel(u"x"); 
-plt.ylabel(u"y"); 
-plt.grid()
-plt.legend(prop={'size':10}, loc='upper right')
-plt.savefig("output.png", bbox_inches='tight')
+## Simple axes
+ax.ylim((-0.1,1.1)); plt.yscale('linear')
+ax.xlim((-0.1,1.1)); plt.xscale('linear')
+
+## Options 
+ax.xlabel(u"x"); 
+ax.ylabel(u"y"); 
+ax.grid()
+ax.legend(prop={'size':10}, loc='upper right')
+fig.tight_layout()
+
+## Show or save
+fig.canvas.mpl_connect('close_event', quit);  fig.show()
+fig.savefig("output.png", bbox_inches='tight')
