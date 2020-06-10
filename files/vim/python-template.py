65c640731c5efbd68b67b373752a3856c6498e9a
Filip Dominec <filip.dominec@gmail.com>
2020-06-10 15:12:12 +0200

    sync config updates from work

--- files/vim/python-template.py
+++ files/vim/python-template.py
@@ -1,28 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/python3  
 #-*- coding: utf-8 -*-
 
 ## Import common moduli
-import matplotlib, sys, os, time
+import matplotlib, sys, os, time, collections
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.constants import c, hbar, pi
+#from scipy.constants import c, hbar, pi
 
 ## Use LaTeX
 matplotlib.rc('text', usetex=True)
 matplotlib.rc('font', size=8)
-matplotlib.rc('text.latex', preamble = '\usepackage{amsmath}, \usepackage{yfonts}, \usepackage{txfonts}, \usepackage{lmodern},')
+#matplotlib.rc('text.latex', preamble = r'\usepackage{amsmath}, \usepackage{yfonts}, \usepackage{txfonts}, \usepackage{lmodern},')
 
+fig, ax = plt.subplots(nrows=2, ncols=1)
+
+## Load data
+#x,y = np.loadtxt(sys.argv[1], unpack=True)
 
 
 
 ## Simple axes
-plt.ylim((-0.1,1.1)); plt.yscale('linear')
-plt.xlim((-0.1,1.1)); plt.xscale('linear')
+ax.ylim((-0.1,1.1)); plt.yscale('linear')
+ax.xlim((-0.1,1.1)); plt.xscale('linear')
 
 ## ==== Outputting ====
 ## Finish the plot + save 
-plt.xlabel(u"x"); 
-plt.ylabel(u"y"); 
-plt.grid()
-plt.legend(prop={'size':10}, loc='upper right')
-plt.savefig("output.png", bbox_inches='tight')
+ax.xlabel(u"x"); 
+ax.ylabel(u"y"); 
+ax.grid()
+ax.legend(prop={'size':10}, loc='upper right')
+ax.savefig("output.png", bbox_inches='tight')
+#np.savetxt(sys.argv[1]+"_corrected.dat", np.array([]).T)
