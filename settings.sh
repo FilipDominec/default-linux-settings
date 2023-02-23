4a96bcea277cc46df61c3b9d1eccf9417b9ee2de
Filip Dominec <filip.dominec@gmail.com>
2023-02-23 15:24:02 +0100

    note on pdf

--- settings.sh
+++ settings.sh
@@ -17,6 +17,7 @@ sudo add-apt-repository -y ppa:inkscape.dev/stable
 sudo apt-get install -y evince gimp inkscape rawtherapee libimage-exiftool-perl gwyddion   rawtherapee hugin 
 sudo apt-get install -y texlive-fonts-extra pdfposter biber texlive-bibtex-extra texlive-lang-czechslovak pdftk imagemagick geeqie djvulibre-bin g3data  #?? pdfjam 
 sudo apt-get install -y texlive-latex-extra dvipng pandoc # for type1cm.sty to make latex+matplotlib work
+#note https://ask.libreoffice.org/t/export-directly-to-pdf-only-produces-a-blank-page/56400 may fix empty PDF problem
 
 ## Multimedia 
 sudo apt-get install -y sound-juicer lame smplayer vlc audacity ffmpeg handbrake
